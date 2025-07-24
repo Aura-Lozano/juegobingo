46e53ee6ab541a87d3120036e4f661c99f650fda
Aura-Lozano <auracristinalozanohoyos@gmail.com>
2025-07-24 17:46:17 -0500

    Primeros cambios

--- codigo/app.js
+++ codigo/app.js
@@ -1,22 +1,14 @@
-angular.module("bingo",[])
-.controller("BingoController", 
-    function($this) {
-    $this.numbers = [];
-    $this.generateNumbers = function() {
-        $this.numbers = [];
-        for (var i = 1; i <= 75; i++) {
-            $this.numbers.push(i);
-        }
-        $this.shuffle($this.numbers);
-    };
+angular.module("bingo", [])
+    .controller("Controladorbingo",
+        function ($scope) {
 
-    $this.shuffle = function(array) {
-        for (let i = array.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            [array[i], array[j]] = [array[j], array[i]];
-        }
-    };
+            $scope.cantor = new Cantor();
+            $scope.iniciarJuego = function () {
 
-    $this.generateNumbers();
-});
-angular.bootstrap(document, ['bingo']); 
+            }
+
+            $scope.sacarBalota = function () {
+                $scope.cantor.sacarBalota(); 
+            }
+        }
+    );
